f12b032425f58f998801ec998c3402d407047391
zer0lightning <42992684+zer0lightning@users.noreply.github.com>
2020-02-24 19:00:27 -0800

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -33,6 +33,6 @@ sleep 2
 ssh-keygen -f /etc/ssh/sh_host_rsa_key -N "" -t rsa
 ssh-keygen -f /etc/ssh/sh_host_dsa_key -N "" -t dsa
 ssh-keygen -f /etc/ssh/ssh_host_key -N ""
-ssh-keygen -A
 sleep 2
+ssh-keygen -A
 /etc/init.d/ssh start
