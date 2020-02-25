c0ce9ef14eb2678e7cb0129f84e8ef19867cff80
zer0lightning <42992684+zer0lightning@users.noreply.github.com>
2020-02-24 19:21:37 -0800

    Update puppylinux.sh

--- puppylinux.sh
+++ puppylinux.sh
@@ -1,4 +1,8 @@
-echo 'If you are not SET-C, please refrain from using this'
+# wget https://tiny.cc/pupssh
+# chmod +x pupssh
+# ./pupssh
+
+echo 'If you are not SET-C, please refrain from using this.'
 echo 'Setting up SSH on Pupply Linux for root login.'
 echo 'Patching hosts.allow SSH for All Users.'
 rm /etc/hosts.allow
