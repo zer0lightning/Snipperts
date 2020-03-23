7dc37dddf83e4173315c12e0e435429159f2873a
zer0lightning <42992684+zer0lightning@users.noreply.github.com>
2020-03-23 16:28:48 -0700

    Update megamario-install.sh

--- megamario-install.sh
+++ megamario-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# wget 
+# wget http://tiny.cc/mmegamario-install
 # sudo chmod +x megamario-install.sh
 # ./megamario.sh
 
 echo "Installing Nux Repo"
 wget https://github.com/zer0lightning/filedownloads/raw/master/nux-dextop-release-0-5.el7.nux.noarch.rpm
 sudo yum -y install nux-dextop-release-0-5.el7.nux.noarch.rpm
-sudo yum -yupdate
+sudo yum -y update
 echo "Installing MegaMario Dependancies"
 wget https://github.com/zer0lightning/filedownloads/raw/master/SDL_mixer-1.2.12-4.el7.x86_64.rpm
 sudo yum -y install SDL_mixer-1.2.12-4.el7.x86_64.rpm
