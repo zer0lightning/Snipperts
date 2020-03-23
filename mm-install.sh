6903215beda2bc05577435ba4fe0a09b1ac18d2d
zer0lightning <42992684+zer0lightning@users.noreply.github.com>
2020-03-23 16:34:09 -0700

    Update mm-install.sh

--- mm-install.sh
+++ mm-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # wget http://tiny.cc/mm-install
-# sudo chmod +x mm-install.sh
-# ./mm-install.sh
+# sudo chmod +x mm-install
+# ./mm-install
 
 echo "Installing Nux Repo"
 wget https://github.com/zer0lightning/filedownloads/raw/master/nux-dextop-release-0-5.el7.nux.noarch.rpm
